407ea5867f5363bdc910b99751b6e9f78f08675e
Isabel Campos Silveira <107281625+IsabelCamposSilveira@users.noreply.github.com>
2024-01-25 20:11:07 -0300

    Add files via upload

---
+++ CRM_Connection.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel.Description;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Crm.Sdk.Messages;
+using Microsoft.Xrm.Sdk.Client;  // Adicionado
+using System.Net;  // Adicionado
+using Microsoft.Xrm.Tooling.Connector;
+using System.Configuration;
+using Microsoft.Xrm.Sdk.Query;
+using System.Windows.Controls;
+
+
+namespace ConsoleAppDynamics
+{
+    public class CRM_Connection
+    {
+        public CrmServiceClient ConectarCRM_Prod()
+        {
+            try
+            {
+                // Dados da conexão
+                string url = ""; // Inserir URL do ambiente
+                string userName = ""; // Inserir e-mail do login
+                string password = ""; // Inserir senha
+
+                string connectionString = $"AuthType=Office365;Url={url};Username={userName};Password={password}";
+
+                // Criando a conexão
+                CrmServiceClient service = new CrmServiceClient(connectionString);
+
+                return service;
+            }             
+            catch (Exception ex)
+            {
+                throw new ArgumentNullException("Falha no Método [ConectarCRM]: " + ex.Message);
+            }         
+
+        }
+
+
+    }
+} 
---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel.Description;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Client;  
+using System.Net;  
+using Microsoft.Xrm.Tooling.Connector;
+using System.Configuration;
+using Microsoft.Xrm.Sdk.Query;
+using System.Windows.Controls;
+using Microsoft.Crm.Sdk.Messages;
+
+namespace ConsoleAppDynamics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Criando a conexão
+            Console.WriteLine("Conectando ao CRM. Aguarde um instante...\n");
+            CRM_Connection connection = new CRM_Connection();
+            CrmServiceClient connectionProd = connection.ConectarCRM_Prod();
+
+            // Chamar as fuñções necessárias
+            RetrieveContact(connectionProd);
+
+
+        }
+
+        public static void RetrieveContact(CrmServiceClient service)
+        {
+
+            // Buscar contatos 
+            var fetchContact = new FetchExpression(@"<fetch version='1.0' output-format='xml-platform' mapping='logical' distinct='true'>
+                                                        <entity name='contact'>
+                                                        <attribute name='firstname'/>
+                                                        <attribute name='lastname'/>                                          
+                                                        </entity>
+                                                    </fetch>");
+
+            var RetrieveContact = service.RetrieveMultiple(fetchContact);
+            
+        }
+
+
+    }
+}
